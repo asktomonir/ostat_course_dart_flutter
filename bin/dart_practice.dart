601a77b54a33ec370fd7e49ad8930408f66daee2
Md Moniruzzaman Sikder <160113519+asktomonir@users.noreply.github.com>
2024-03-02 15:58:02 +0600

    Initial commit

---
+++ bin/dart_practice.dart
@@ -0,0 +1,58 @@
+main(){
+  // Integer = 1,2,3,4  ---> int
+  // Floating = 1.12, 12.55  --> Double
+  // String / textual data = a, b, c, 'Rafat' "ostad' -- String
+  // boolean = true / false ---> bool
+  // container
+  // variable creation syntax
+  // data-type    variable - name = value;
+  // compound datatype : List, Maps,
+  // Integer is a full number
+  // Double contain Double
+
+
+int firstNumber= 10; // camelcase
+int secondNumber= 20;
+int additionResult= firstNumber + secondNumber;
+
+print(additionResult);
+
+
+  firstNumber=20;
+  print(firstNumber);
+
+  firstNumber=100;
+  print(firstNumber);
+;
+
+
+  print ("Hello Dart I love you");
+  print('Rafat');
+  print(12+12);
+// double
+double cGpa = 3.501;
+
+print(cGpa);
+
+double myFriendsCGpa = 3.92;
+
+double total = cGpa + myFriendsCGpa;
+print(total);
+
+
+
+// String
+
+String myName = 'Sunny';
+String studentName = "ABC";
+
+print(myName);
+print(studentName);
+
+// Premetive DataType
+// Boolean
+bool isStudent = false;
+print(isStudent);
+
+
+}
